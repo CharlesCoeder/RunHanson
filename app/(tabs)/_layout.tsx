d5bcc3f4ebe32a77866790ea3176d72ed4105694
CharlesCoeder <croederdev@gmail.com>
2024-04-06 20:16:49 -0400

    Tab navigation

---
+++ app/(tabs)/_layout.tsx
@@ -0,0 +1,48 @@
+import React from "react";
+import FontAwesome from "@expo/vector-icons/FontAwesome";
+import { Tabs } from "expo-router";
+
+export default function TabLayout() {
+  return (
+    <Tabs
+      screenOptions={{ tabBarActiveTintColor: "orange", headerShown: false }}
+    >
+      <Tabs.Screen
+        name="index"
+        options={{
+          title: "Home",
+          tabBarIcon: ({ color }) => (
+            <FontAwesome size={28} name="home" color={color} />
+          ),
+        }}
+      />
+      <Tabs.Screen
+        name="schedule"
+        options={{
+          title: "Schedule",
+          tabBarIcon: ({ color }) => (
+            <FontAwesome size={28} name="calendar-o" color={color} />
+          ),
+        }}
+      />
+      <Tabs.Screen
+        name="runLog"
+        options={{
+          title: "Run Log",
+          tabBarIcon: ({ color }) => (
+            <FontAwesome size={28} name="home" color={color} />
+          ),
+        }}
+      />
+      <Tabs.Screen
+        name="settings"
+        options={{
+          title: "Settings",
+          tabBarIcon: ({ color }) => (
+            <FontAwesome size={28} name="cog" color={color} />
+          ),
+        }}
+      />
+    </Tabs>
+  );
+}
---
+++ app/(tabs)/schedule.tsx
@@ -0,0 +1,9 @@
+import { View, Text } from "react-native";
+
+export default function Schedule() {
+  return (
+    <View style={{ justifyContent: "center", alignItems: "center", flex: 1 }}>
+      <Text>Schedule</Text>
+    </View>
+  );
+}
