6ac00816ac6f945dad82b2111f4eac3ac8b972dc
CharlesCoeder <croederdev@gmail.com>
2024-04-12 00:44:43 -0400

    Remove headers

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -4,6 +4,8 @@ export default function AppLayout() {
   return (
     <Stack>
       <Stack.Screen name="(tabs)" options={{ headerShown: false }} />
+      <Stack.Screen name="index" options={{ headerShown: false }} />
+      <Stack.Screen name="auth/login/index" options={{ headerShown: false }} />
     </Stack>
   );
 }
